ec054fa91aacc0bba9ea5204ecea08dbf1704454
HARIESHV <harieshvenkatachalam@gmail.com>
2026-04-26 12:49:01 +0530

    Bypass PEP 668 to allow pip install on Render

--- run_prod.py
+++ run_prod.py
@@ -3,12 +3,12 @@ import sys
 
 # FOOLPROOF HACK: Force install requirements before starting
 # This bypasses Render if the Build Command is misconfigured
-os.system(f"{sys.executable} -m pip install -r requirements.txt")
+os.system(f"{sys.executable} -m pip install -r requirements.txt --break-system-packages")
 
 try:
     from waitress import serve
 except ImportError:
-    os.system(f"{sys.executable} -m pip install waitress")
+    os.system(f"{sys.executable} -m pip install waitress --break-system-packages")
     from waitress import serve
 from backend.app import app
 
