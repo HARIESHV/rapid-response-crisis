5fe301312efc3ad884a22c8f71383a1ea6ceaca2
HARIESHV <harieshvenkatachalam@gmail.com>
2026-04-26 12:53:02 +0530

    Fix python sys.path for Render user site-packages

--- run_prod.py
+++ run_prod.py
@@ -1,9 +1,13 @@
 import os
 import sys
+import glob
 
 # FOOLPROOF HACK: Force install requirements before starting
-# This bypasses Render if the Build Command is misconfigured
 os.system(f"{sys.executable} -m pip install -r requirements.txt --break-system-packages")
+
+# HACK 2: Ensure the user site-packages is in sys.path so python can find the installed packages
+local_paths = glob.glob(os.path.expanduser("~/.local/lib/python*/site-packages"))
+sys.path.extend(local_paths)
 
 try:
     from waitress import serve
